a7f7d7a731c4bc3fc77a1e079b2329f8941e5b94
Alex Harms <onealexharms@gmail.com>
2022-12-31 13:39:02 -0500

    There are 80 glyphs

--- font/import_font.py
+++ font/import_font.py
@@ -1,12 +1,18 @@
 import os
+import unittest
 
-def main():
-    glyphs = [
-        file.removesuffix('.svg')
-        for file in os.listdir('font/glyphs')
-        if file.endswith('.svg')
-    ]
-    print(glyphs)
+class FontMaker:
+    def __init__(self):
+        self.glyphs = [
+            file.removesuffix('.svg')
+            for file in os.listdir('font/glyphs')
+            if file.endswith('.svg')
+        ]
+
+class TheTests(unittest.TestCase):
+    def test_there_are_80_glyphs(self):
+        self.assertEqual(len(FontMaker().glyphs), 80)
 
 if __name__=='__main__':
-   main()
+    unittest.main()
+    main()
