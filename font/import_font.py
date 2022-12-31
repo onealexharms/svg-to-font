a4c84136745cc8b8cd05cb15c28efabc935ea08d
Alex Harms <onealexharms@gmail.com>
2022-12-31 10:27:41 -0500

    Don't include .DS_Store

--- font/import_font.py
+++ font/import_font.py
@@ -1,89 +1,15 @@
-LETTERS = [
-    'B/bhu.svg',
-    'B/bha.svg',
-    'B/ibh.svg',
-    'B/ubh.svg',
-    'B/bhai.svg',
-    'B/aibh.svg',
-    'B/bhi.svg',
-    'B/abh.svg',
-    'D/dhu.svg',
-    'D/dha.svg',
-    'D/udh.svg',
-    'D/idh.svg',
-    'D/aidh.svg',
-    'D/dhai.svg',
-    'D/adh.svg',
-    'D/dhi.svg',
-    'DJ/udj.svg',
-    'DJ/dji.svg',
-    'DJ/idj.svg',
-    'DJ/adj.svg',
-    'DJ/djai.svg',
-    'DJ/aidj.svg',
-    'DJ/dju.svg',
-    'DJ/dja.svg',
-    'G/ga.svg',
-    'G/gai.svg',
-    'G/gu.svg',
-    'G/aig.svg',
-    'G/ug.svg',
-    'G/ag.svg',
-    'G/gi.svg',
-    'G/ig.svg',
-    'K/alh.svg',
-    'K/ik.svg',
-    'K/Khai.svg',
-    'K/khi.svg',
-    'K/aikh.svg',
-    'K/Kha.svg',
-    'K/khu.svg',
-    'K/uk.svg',
-    'L/il.svg',
-    'L/lu.svg',
-    'L/la.svg',
-    'L/lai.svg',
-    'L/ul.svg',
-    'L/ail.svg',
-    'L/al.svg',
-    'L/li.svg',
-    'M/im.svg',
-    'M/ma.svg',
-    'M/mu.svg',
-    'M/mi.svg',
-    'M/aim.svg',
-    'M/um.svg',
-    'M/am.svg',
-    'M/mai.svg',
-    'T/tu.svg',
-    'T/at.svg',
-    'T/ta.svg',
-    'T/ut.svg',
-    'T/ait.svg',
-    'T/ti.svg',
-    'T/it.svg',
-    'T/tai.svg',
-    'TCH/tcha.svg',
-    'TCH/aitch.svg',
-    'TCH/tcha_2.svg',
-    'TCH/itch.svg',
-    'TCH/tchai.svg',
-    'TCH/utch.svg',
-    'TCH/atch.svg',
-    'TCH/tchi.svg',
-    'Z/izh.svg',
-    'Z/uzh.svg',
-    'Z/zhu.svg',
-    'Z/zha.svg',
-    'Z/zhi.svg',
-    'Z/az.svg',
-    'Z/zhai.svg',
-    'Z/aizh.svg',
-]
+import os
+
+CONSONANTS = [ 'B', 'D', 'DJ', 'G', 'K', 'L', 'M', 'T', 'TCH', 'Z' ]
 
 def main():
-    for letter in LETTERS:
-        print("file is", letter)
+    for consonant in CONSONANTS:
+        files = [
+            file
+            for file in os.listdir('font/' + consonant)
+            if file.endswith('.svg')
+        ]
+        print(files)
 
 if __name__=='__main__':
    main() 
