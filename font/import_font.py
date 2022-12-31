77d2f17921718e2094fe5dfdd1933d38f1eb4a7b
Alex Harms <onealexharms@gmail.com>
2022-12-31 10:34:49 -0500

    Move everything to letters

--- font/import_font.py
+++ font/import_font.py
@@ -3,13 +3,13 @@ import os
 CONSONANTS = [ 'B', 'D', 'DJ', 'G', 'K', 'L', 'M', 'T', 'TCH', 'Z' ]
 
 def main():
-    for consonant in CONSONANTS:
-        files = [
-            file
-            for file in os.listdir('font/' + consonant)
-            if file.endswith('.svg')
-        ]
-        print(files)
+    files = [
+        f'font/{consonant}/{file}'
+        for consonant in CONSONANTS
+        for file in os.listdir('font/' + consonant)
+        if file.endswith('.svg')
+    ]
+    print(files)
 
 if __name__=='__main__':
    main() 
