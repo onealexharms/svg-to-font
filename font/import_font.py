aa58c10d6c9c85bf4c077b34f5c753daece217eb
Alex Harms <onealexharms@gmail.com>
2022-12-31 14:09:07 -0500

    Each glyph has an openable svg_file

--- font/import_font.py
+++ font/import_font.py
@@ -4,6 +4,7 @@ import unittest
 class Glyph:
     def __init__(self, name):
         self.name = name 
+        self.svg_file = f'font/glyphs/{name}.svg'
 
 class FontMaker:
     def __init__(self):
@@ -22,12 +23,16 @@ class Tests(unittest.TestCase):
         for glyph in glyphs:
             self.assertIsInstance(glyph.name, str)
         unique_names = set([glyph.name for glyph in glyphs]) 
-        self.assertEquals(len(unique_names), len(glyphs))
+        self.assertEqual(len(unique_names), len(glyphs))
 
     def test_we_have_a_glyph_named_tcha(self):
         found = [glyph for glyph in FontMaker().glyphs if glyph.name == 'tcha']
-        self.assertEquals(len(found), 1)
+        self.assertEqual(len(found), 1)
 
+    def test_each_glyph_has_an_openable_svg_file(self):
+        for glyph in FontMaker().glyphs:
+            with open(glyph.svg_file) as f:
+                pass
 
 if __name__=='__main__':
     unittest.main()
